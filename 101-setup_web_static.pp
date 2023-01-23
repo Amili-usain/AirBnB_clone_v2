78f20847b99486841a78b4d03133fa5a7764ed74
Amili-usain <amilihussein073@gmail.com>
2023-01-23 12:14:43 +0000

    fix

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,62 +1,78 @@
-# Script that configures Nginx server with some folders and files
-
-exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+# Manifest that configures a web server for deployment
+exec { 'exec_0':
+  command => 'sudo sudo apt-get update -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['start Nginx'],
+exec { 'exec_1':
+  require => Exec['exec_0'],
+  command => 'sudo apt-get install nginx -y',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-exec {'start Nginx':
-  provider => shell,
-  command  => 'sudo service nginx start',
-  before   => Exec['create first directory'],
+exec { 'exec_2':
+  require => Exec['exec_1'],
+  command => 'sudo mkdir -p /data/web_static/shared/',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-exec {'create first directory':
-  provider => shell,
-  command  => 'sudo mkdir -p /data/web_static/releases/test/',
-  before   => Exec['create second directory'],
+exec { 'exec_3':
+  require => Exec['exec_2'],
+  command => 'sudo mkdir -p /data/web_static/releases/test/',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-exec {'create second directory':
-  provider => shell,
-  command  => 'sudo mkdir -p /data/web_static/shared/',
-  before   => Exec['content into html'],
+exec { 'exec_4':
+  require => Exec['exec_3'],
+  command => 'sudo touch /data/web_static/releases/test/index.html',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-exec {'content into html':
-  provider => shell,
-  command  => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
-  before   => Exec['symbolic link'],
+exec { 'exec_5':
+  require => Exec['exec_4'],
+  command => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html >/dev/null',
+  path    => ['/usr/bin', '/bin'],
+  returns => [0,1]
 }
 
-exec {'symbolic link':
-  provider => shell,
-  command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  before   => Exec['put location'],
+exec { 'exec_6':
+  require => Exec['exec_5'],
+  command => 'rm -rf /data/web_static/current',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
 
-exec {'put location':
-  provider => shell,
-  command  => 'sudo sed -i \'38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\' /etc/nginx/sites-available/default',
-  before   => Exec['restart Nginx'],
+
+exec { 'exec_7':
+  require => Exec['exec_6'],
+  command => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
 
-exec {'restart Nginx':
-  provider => shell,
-  command  => 'sudo service nginx restart',
-  before   => File['/data/']
+exec { 'exec_8':
+  require => Exec['exec_7'],
+  command => 'sudo chown -R ubuntu:ubuntu /data/',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }
 
-file {'/data/':
-  ensure  => directory,
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  recurse => true,
+exec { 'exec_9':
+  require     => Exec['exec_8'],
+  environment => ['C=\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n'],
+  command     => 'sudo sed -i "38i $C" /etc/nginx/sites-available/default',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0,1]
+}
+
+exec { 'exec_10':
+  require => Exec['exec_9'],
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/bin', '/usr/sbin'],
+  returns => [0,1]
 }

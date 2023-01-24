cd9f1a4952f6b2208784582db4d3055a794aa365
Amili-usain <amilihussein073@gmail.com>
2023-01-24 02:00:43 -0800

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,60 +1,39 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
-from datetime import datetime
-from fabric.api import env, local, put, run, runs_once
+"""
+    Distributes an archive to your web servers,
+    using the function do_deploy
+    def do_deploy(archive_path):
+    Return False iff archive path doesn't exist
+"""
 
-
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['54.173.26.191', '54.237.65.84']
-The list of host server IP addresses."""
-
-
-@runs_once
-def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
+env.user = 'ubuntu'
+env.identity = '~/.ssh/school'
+env.password = None
 
 
 def do_deploy(archive_path):
-    """Deploys the static files to the host servers.
-    Args:
-        archive_path (str): The path to the archived static files.
     """
-    if not os.path.exists(archive_path):
+    Deploys an archive to a server
+    """
+    if exists(archive_path) is False:
         return False
-    file_name = os.path.basename(archive_path)
-    folder_name = file_name.replace(".tgz", "")
-    folder_path = "/data/web_static/releases/{}/".format(folder_name)
-    success = False
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-        run("rm -rf /tmp/{}".format(file_name))
-        run("mv {}web_static/* {}".format(folder_path, folder_path))
-        run("rm -rf {}web_static".format(folder_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_path))
-        print('New version deployed!')
-        success = True
-    except Exception:
-        success = False
-    return success
+        file_N = archive_path.split("/")[-1]
+        n = file_N.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, n))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_N, path, n))
+        run('rm /tmp/{}'.format(file_N))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, n))
+        run('rm -rf {}{}/web_static'.format(path, n))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, n))
+        run('chmod -R 755 /data/')
+        print("New version deployed!")
+        return True
+    except FileNotFoundError:
+        return False
